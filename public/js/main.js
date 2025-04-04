f8811905038acbe848c7b23c4e377b6779f3ad5e
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-04-04 11:55:53 +0200

    upate

--- public/js/main.js
+++ public/js/main.js
@@ -15,13 +15,13 @@ updateTime(); // calling the function for the first time so that it does not sho
 
 
 function SetLampje(ledNum, inOn) {
-    url = ("https://38406.hosts2.ma-cloud.nl/EcoNest/api/post.php?led" + ledNum + "=" + inOn);
+    url = ("https://38734.hosts2.ma-cloud.nl/EcoNest/api/post.php?led" + ledNum + "=" + inOn);
     console.log(url);
     fetch(url);
 }
 
 function GetData() {
-    return fetch("https://38406.hosts2.ma-cloud.nl/EcoNest/api/post.php")
+    return fetch("https://38734.hosts2.ma-cloud.nl/EcoNest/api/post.php")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
