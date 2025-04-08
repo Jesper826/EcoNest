fc22bfdfc42d30e02e00b1d801819cea72c7ff14
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-04-08 15:33:20 +0200

    Update aanofuit.js

--- public/js/aanofuit.js
+++ public/js/aanofuit.js
@@ -1,15 +1,13 @@
 function checkTimeAndSetLampje() {
     const now = new Date();
-    const targetHour = 15; // 3 uur 's middags
+    const targetHour = 15; 
 
-    // Controleer of het 3 uur of later is
     if (now.getHours() >= targetHour) {
         GetData().then(data => {
             console.log("JSON data:", data);
 
-            // Controleer of LED2 uit staat
             if (!data.lights.LED2) {
-                SetLampje(2, true); // Zet LED2 aan
+                 SetLampje(2, true); 
                 console.log("Lampje 2 is AAN gezet om 3 uur of later.");
             } else {
                 console.log("Lampje 2 stond al aan.");
