7b5c8b7ca8e0775255c56c519e2a1574e52e006d
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-04-11 10:23:29 +0200

    c

--- public/js/ldr.js
+++ public/js/ldr.js
@@ -1,24 +1,23 @@
 function updateLDRStatus() {
     const ldrValueElement = document.getElementById("ldrValue");
-    ldrValueElement.className = "loading"; // Voeg de 'loading'-klasse toe
+    ldrValueElement.className = "loading"; 
 
     GetData().then(data => {
         if (data && data.ldr !== null) {
-            const ldrValue = data.ldr; // Haal de LDR-waarde op uit de JSON
+            const ldrValue = data.ldr;
             ldrValueElement.textContent = `Lichtintensiteit: ${ldrValue}`;
-            ldrValueElement.className = ""; // Verwijder de 'loading'-klasse
+            ldrValueElement.className = ""; 
         } else {
             console.error("Kon geen LDR-gegevens ophalen.");
             ldrValueElement.textContent = "LDR-status niet beschikbaar.";
-            ldrValueElement.className = "error"; // Voeg de 'error'-klasse toe
+            ldrValueElement.className = "error"; 
         }
     }).catch(error => {
         console.error("Fout bij het ophalen van LDR-gegevens:", error);
         ldrValueElement.textContent = "Fout bij ophalen.";
-        ldrValueElement.className = "error"; // Voeg de 'error'-klasse toe
+        ldrValueElement.className = "error"; 
     });
 }
 
-// Update de LDR-status elke seconde
 setInterval(updateLDRStatus, 1000);
 updateLDRStatus();
